f4e909201fd75b45744abd29c104d86ca4045054
AtobaAzul <atoba.azul.radical@gmail.com>
2024-10-29 19:38:00 -0300

    Fix tfc's rain not properly interacting with cold sweat

---
+++ src/main/java/net/atobaazul/tfc_coldsweat/mixin/WorldHelperMixin.java
@@ -0,0 +1,29 @@
+package net.atobaazul.tfc_coldsweat.mixin;
+
+import com.momosoftworks.coldsweat.util.compat.CompatManager;
+import com.momosoftworks.coldsweat.util.serialization.DynamicHolder;
+import com.momosoftworks.coldsweat.util.world.WorldHelper;
+import net.dries007.tfc.util.EnvironmentHelpers;
+import net.dries007.tfc.util.climate.Climate;
+import net.dries007.tfc.util.tracker.WeatherHelpers;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.biome.Biome;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import static com.momosoftworks.coldsweat.util.world.WorldHelper.canSeeSky;
+
+
+//Technically breaks Weather compat, but, uh, too bad!
+@Mixin(WorldHelper.class)
+public class WorldHelperMixin {
+    @Inject(method="isRainingAt", at=@At("HEAD"), remap = false, cancellable = true)
+    private static void tfc_coldsweat$isRainingAt(Level level, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+        {
+            cir.setReturnValue(EnvironmentHelpers.isRainingOrSnowing(level, pos) && Climate.getPrecipitation(level, pos) == Biome.Precipitation.RAIN);
+        }
+    }
+}
