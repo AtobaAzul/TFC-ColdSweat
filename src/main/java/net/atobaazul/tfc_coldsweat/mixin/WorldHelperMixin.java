6b7db1f166ed59099cc869dd64bd959d68e81554
AtobaAzul <atoba.azul.radical@gmail.com>
2024-10-30 13:44:19 -0300

    Fixed isRainingAt not checking if the player is inside

--- src/main/java/net/atobaazul/tfc_coldsweat/mixin/WorldHelperMixin.java
+++ src/main/java/net/atobaazul/tfc_coldsweat/mixin/WorldHelperMixin.java
@@ -8,8 +8,10 @@ import net.dries007.tfc.util.climate.Climate;
 import net.dries007.tfc.util.tracker.WeatherHelpers;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.biome.Biome;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -19,11 +21,12 @@ import static com.momosoftworks.coldsweat.util.world.WorldHelper.canSeeSky;
 
 //Technically breaks Weather compat, but, uh, too bad!
 @Mixin(WorldHelper.class)
-public class WorldHelperMixin {
+public abstract class WorldHelperMixin {
     @Inject(method="isRainingAt", at=@At("HEAD"), remap = false, cancellable = true)
     private static void tfc_coldsweat$isRainingAt(Level level, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
         {
-            cir.setReturnValue(EnvironmentHelpers.isRainingOrSnowing(level, pos) && Climate.getPrecipitation(level, pos) == Biome.Precipitation.RAIN);
+            cir.setReturnValue(EnvironmentHelpers.isRainingOrSnowing(level, pos) && Climate.getPrecipitation(level, pos) == Biome.Precipitation.RAIN
+                    && canSeeSky(level, pos, level.getMaxBuildHeight()));
         }
     }
 }
