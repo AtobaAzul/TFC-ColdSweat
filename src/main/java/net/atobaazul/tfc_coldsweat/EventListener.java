c59960fd60dac0431a20b01a7131d7a9a40d8c85
AtobaAzul <atoba.azul.radical@gmail.com>
2024-11-29 18:18:40 -0300

    Dynamic grace duration based on temperature

--- src/main/java/net/atobaazul/tfc_coldsweat/EventListener.java
+++ src/main/java/net/atobaazul/tfc_coldsweat/EventListener.java
@@ -8,20 +8,49 @@ import com.momosoftworks.coldsweat.api.temperature.modifier.TempModifier;
 import com.momosoftworks.coldsweat.api.temperature.modifier.UndergroundTempModifier;
 import com.momosoftworks.coldsweat.api.util.Placement;
 import com.momosoftworks.coldsweat.api.util.Temperature;
+import com.momosoftworks.coldsweat.config.ConfigSettings;
+import com.momosoftworks.coldsweat.util.registries.ModEffects;
 import net.atobaazul.tfc_coldsweat.block_temp.*;
 import net.atobaazul.tfc_coldsweat.inv_temp.HotItemsTempModifier;
 import net.atobaazul.tfc_coldsweat.modifier.ClimateTempModifier;
 import net.atobaazul.tfc_coldsweat.modifier.SunlightTempModifier;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber
 public class EventListener {
     public final static TempModifier TFCSeasonModifier = new ClimateTempModifier();
     public static final TempModifier TFCSunlightModifier = new SunlightTempModifier();
+
+    private static double getSeasonalGraceDuration(Level level, Player player) {
+        int baseDuration = ConfigSettings.GRACE_LENGTH.get();
+        double baseTemp = Temperature.convert(27, Temperature.Units.C, Temperature.Units.MC, true);
+        double tempAtSpawn = Temperature.getTemperatureAt(player.blockPosition(), level);
+
+        double tempPercent = baseTemp / tempAtSpawn;
+        if (tempPercent < 1) {
+            tempPercent = 1 + (1 - tempPercent);
+        }
+
+        tempPercent = Math.min(tempPercent, 2);
+        return baseDuration * tempPercent;
+    };
+
+    @SubscribeEvent
+    public static void onSpawn(EntityJoinLevelEvent event) {
+        if (!event.getLevel().isClientSide && event.getEntity() instanceof Player
+                && ConfigSettings.GRACE_ENABLED.get() && !event.getEntity().getPersistentData().getBoolean("GivenGracePeriod")) {
+            event.getEntity().getPersistentData().putBoolean("GivenGracePeriod", true);
+            ((Player) event.getEntity()).addEffect(new MobEffectInstance(ModEffects.GRACE, (int) getSeasonalGraceDuration(event.getLevel(), (Player) event.getEntity()), 0, false, false, true));
+        }
+    }
+
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public static void defineDefaultModifiers(GatherDefaultTempModifiersEvent event) {
