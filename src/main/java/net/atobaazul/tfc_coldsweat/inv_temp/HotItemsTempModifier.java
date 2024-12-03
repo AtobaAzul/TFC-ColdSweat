cc31a943fa6e337f9ffb7289e81dc029ed3ea9f5
AtobaAzul <atoba.azul.radical@gmail.com>
2024-12-03 17:08:19 -0300

    Hot or Not compat

--- src/main/java/net/atobaazul/tfc_coldsweat/inv_temp/HotItemsTempModifier.java
+++ src/main/java/net/atobaazul/tfc_coldsweat/inv_temp/HotItemsTempModifier.java
@@ -3,12 +3,20 @@ package net.atobaazul.tfc_coldsweat.inv_temp;
 import com.momosoftworks.coldsweat.api.temperature.modifier.InventoryItemsTempModifier;
 import com.momosoftworks.coldsweat.api.util.Temperature;
 import net.dries007.tfc.common.capabilities.heat.HeatCapability;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 
 import java.util.function.Function;
 
 public class HotItemsTempModifier extends InventoryItemsTempModifier {
+    TagKey<Item> insulatingTag = TagKey.create(
+            BuiltInRegistries.ITEM.key(),
+            new ResourceLocation("tfchotornot", "insulating"));
+
     @Override
     protected Function<Double, Double> calculate(LivingEntity entity, Temperature.Trait trait)
     {
@@ -16,7 +24,8 @@ public class HotItemsTempModifier extends InventoryItemsTempModifier {
         if (entity instanceof Player) {
             ((Player) entity).getInventory().items.forEach(item -> {
                 if (HeatCapability.getTemperature(item) > 0 && totalHeat[0] < 1) {
-                    double itemTemp = Temperature.convert(HeatCapability.getTemperature(item), Temperature.Units.C, Temperature.Units.MC, false)/40;
+                    double heatValue = entity.getOffhandItem().is(insulatingTag) ? 80 : 40;
+                    double itemTemp = Temperature.convert(HeatCapability.getTemperature(item), Temperature.Units.C, Temperature.Units.MC, false)/heatValue;
                     totalHeat[0] = (float) (totalHeat[0] + itemTemp);
                 } else if (totalHeat[0] > 1) {
                     totalHeat[0] = 1;
