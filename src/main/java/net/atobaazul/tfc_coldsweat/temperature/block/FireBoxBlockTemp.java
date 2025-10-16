97d9da5ea9a47b7c69ebe2df242abc6b0ff0325b
AtobaAzul <atoba.azul.radical@gmail.com>
2025-10-16 12:10:03 -0300

    Fixed Firebox not giving off heat

--- src/main/java/net/atobaazul/tfc_coldsweat/temperature/block/FireBoxBlockTemp.java
+++ src/main/java/net/atobaazul/tfc_coldsweat/temperature/block/FireBoxBlockTemp.java
@@ -3,7 +3,7 @@ package net.atobaazul.tfc_coldsweat.temperature.block;
 import com.momosoftworks.coldsweat.api.temperature.block_temp.BlockTemp;
 import com.momosoftworks.coldsweat.api.util.Temperature;
 import com.momosoftworks.coldsweat.util.math.CSMath;
-import net.dries007.tfc.common.blockentities.CharcoalForgeBlockEntity;
+import net.dries007.tfc.common.blockentities.FireboxBlockEntity;
 import net.dries007.tfc.common.blocks.TFCBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.LivingEntity;
@@ -19,8 +19,8 @@ public class FireBoxBlockTemp extends BlockTemp {
     @Override
     public double getTemperature(Level level, LivingEntity entity, BlockState state, BlockPos pos, double distance) {
         BlockEntity be = level.getBlockEntity(pos);
-        if (be instanceof CharcoalForgeBlockEntity) {
-            return CSMath.blend(Temperature.convert(((CharcoalForgeBlockEntity) be).getTemperature(), Temperature.Units.C, Temperature.Units.MC, true), 0, distance, 0.5, 16) / 30;
+        if (be instanceof FireboxBlockEntity) {
+            return CSMath.blend(Temperature.convert(((FireboxBlockEntity) be).getTemperature(), Temperature.Units.C, Temperature.Units.MC, true), 0, distance, 0.5, 16) / 30;
         }
         return 0.0;
     }
