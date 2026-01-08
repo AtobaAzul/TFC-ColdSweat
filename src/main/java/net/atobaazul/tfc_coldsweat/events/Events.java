2c5ec67a0925732751a94d0a6a8a5aebd15acdb0
AtobaAzul <atoba.azul.radical@gmail.com>
2026-01-08 11:44:07 -0300

    Disable CS`s shade temp modifier

--- src/main/java/net/atobaazul/tfc_coldsweat/events/Events.java
+++ src/main/java/net/atobaazul/tfc_coldsweat/events/Events.java
@@ -68,7 +68,7 @@ public class Events {
             if (CompatManager.TFC_ENABLED != null) {
                 event.addModifier(Temperature.Trait.WORLD, TFCSeasonModifier, Placement.LAST.noDuplicates(Matcher.SAME_CLASS));
                 event.addModifier(Temperature.Trait.CORE, ItemTempModifier, Placement.LAST.noDuplicates(Matcher.SAME_CLASS));
-                event.getModifiers(Temperature.Trait.WORLD).removeIf(modifier -> modifier instanceof BiomeTempModifier || modifier instanceof CaveBiomeTempModifier || modifier instanceof ElevationTempModifier);
+                event.getModifiers(Temperature.Trait.WORLD).removeIf(modifier -> modifier instanceof BiomeTempModifier || modifier instanceof CaveBiomeTempModifier || modifier instanceof ElevationTempModifier || modifier instanceof ShadeTempModifier);
             }
         }
     }
